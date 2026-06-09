5b9cc7f1a5669fd178224d9b287f2bba3aaf689b
Akram-Dwf <akramtamir12@gmail.com>
2026-06-09 12:17:14 +0800

    feat: setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+
+    // Picasso untuk load gambar dari URL
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    // Material Design
+    implementation("com.google.android.material:material:1.9.0")
 }
